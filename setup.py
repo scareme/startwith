ea8b68e74df4825c2aad0f2ace25c4b48a2714d4
Kirill Ivanov <39565112+scareme@users.noreply.github.com>
2023-04-20 16:39:53 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!\usr\bin\python
+"""Setuptools-based installation."""
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+readme = Path(__file__).parent / "README.md"
+
+
+with Path("requirements.txt").open() as file:
+    dependencies = [line.rstrip() for line in file if not line.startswith("-") and "@git" not in line]
+
+
+setup(
+    name="src",
+    packages=find_packages(exclude=["tests"]),
+    author="Me",
+    version="1.0",
+    include_package_data=True,
+    install_requires=dependencies
+)
