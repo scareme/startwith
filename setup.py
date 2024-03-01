5e337ad6e9b700b10d73998c167a044772f9c642
Kirill Ivanov <s.kirill.iv@gmail.com>
2024-03-01 21:48:38 +0300

    vscode new version supported

--- setup.py
+++ setup.py
@@ -4,18 +4,17 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-from startwith.version import __version__
+from src.version import __version__
 
 readme = Path(__file__).parent / "README.md"
-
 
 with Path("requirements.txt").open() as file:
     dependencies = [line.rstrip() for line in file if not line.startswith("-") and "@git" not in line]
 
 
 setup(
-    name="startwith",
-    packages=find_packages(include=["startwith"]),
+    name="src",
+    packages=find_packages(include=["src"]),
     author="Kirill Ivanov",
     version=__version__,
     include_package_data=True,
