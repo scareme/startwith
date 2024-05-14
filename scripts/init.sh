cd9777588026acccc58a18d904a7983eeae36700
Kirill Ivanov <s.kirill.iv@gmail.com>
2024-05-14 20:37:57 +0300

    script.sh fixed

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,6 +3,6 @@ mv startwith "$1"
 cd $1
 sed -i '' "s/dev-startwith/$1-dev/" environment.yml
 sed -i '' "s/dev-startwith/$1-dev/" README.md
-git remove origin
+git remote remove origin
 mv .vscode/startwith.code-workspace ".vscode/$1.code-workspace"
 code .vscode/startwith.code-workspace
