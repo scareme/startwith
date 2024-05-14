0a962d45a73580f363ed1ededaf489ddeb5f41f5
Kirill Ivanov <s.kirill.iv@gmail.com>
2024-05-14 20:42:29 +0300

    rm -rf .git added

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,5 +4,6 @@ cd $1
 sed -i '' "s/dev-startwith/$1-dev/" environment.yml
 sed -i '' "s/dev-startwith/$1-dev/" README.md
 git remote remove origin
+rm -rf .git --yes
 mv .vscode/startwith.code-workspace ".vscode/$1.code-workspace"
 code .vscode/startwith.code-workspace
